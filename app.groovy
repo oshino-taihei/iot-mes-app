40b27d39d3896f442382118413211cf1fbfdc8e6
taihei.oshino <oshino.taihei+github@gmail.com>
2017-03-17 15:36:50 +0900

    エラーハンドリングとメッセージ送信・失敗を画面に表示するようにした

--- app.groovy
+++ app.groovy
@@ -9,10 +9,10 @@ import org.slf4j.LoggerFactory
 
 @Controller
 class IoTMesApp {
-    private static final Logger logger = LoggerFactory.getLogger(IoTMesApp.class);
-    private static final String URN_DEVICE = "urn:Accenture:JPTechForMES";
-    private static final String PROVISIONING_FILE_PATH = "config/JPTechDummyForMES-provisioning-file.conf";
-    private static final String PROVISIONING_FILE_PASS = "PYBSq85bwlIlh2vF";
+    private static final Logger logger = LoggerFactory.getLogger(IoTMesApp.class)
+    private static final String URN_DEVICE = "urn:Accenture:JPTechForMES"
+    private static final String PROVISIONING_FILE_PATH = "config/JPTechDummyForMES-provisioning-file.conf"
+    private static final String PROVISIONING_FILE_PASS = "PYBSq85bwlIlh2vF"
 
     @RequestMapping("/")
     String home() {
@@ -21,16 +21,22 @@ class IoTMesApp {
 
     @RequestMapping("/submit")
     String submit(@RequestParam Map<String, String> params) {
-      DirectlyConnectedDevice device = new DirectlyConnectedDevice(PROVISIONING_FILE_PATH, PROVISIONING_FILE_PASS);
-      if (!device.isActivated()) {
-        device.activate(URN_DEVICE);
+      try {
+        DirectlyConnectedDevice device = new DirectlyConnectedDevice(PROVISIONING_FILE_PATH, PROVISIONING_FILE_PASS)
+        if (!device.isActivated()) {
+          device.activate(URN_DEVICE)
+        }
+        def messageBuilder = new DataMessage.Builder().format("${URN_DEVICE}:attributes").source(device.getEndpointId())
+        params.each { key, value -> messageBuilder.dataItem(key, value) }
+        DataMessage message = messageBuilder.build()
+        device.send(message)
+        device.close()
+        logger.info("finished sending the message. params=${params}")
+        return "redirect:/?success"
+      } catch (Exception e) {
+        logger.error("sending the message was failed. params=${params} exception=[${e}]")
+        e.printStackTrace()
+        return "redirect:/?error"
       }
-      def messageBuilder = new DataMessage.Builder().format("${URN_DEVICE}:attributes").source(device.getEndpointId())
-      params.each { key, value -> messageBuilder.dataItem(key, value) }
-      DataMessage message = messageBuilder.build()
-      device.send(message)
-      device.close()
-      logger.info("finished sending the message. params=${params}")
-      "redirect:/"
     }
 }
